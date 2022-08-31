b609f6fbc21f7af29b3d524c81ca7c5cda910183
Nihad Talal <nihadtalal@gmail.com>
2022-08-31 23:19:08 +0300

    add logout

--- vue/src/components/DefaultLayout.vue
+++ vue/src/components/DefaultLayout.vue
@@ -21,11 +21,7 @@
                     </div>
                     <div class="hidden md:block">
                         <div class="ml-4 flex items-center md:ml-6">
-                            <button type="button"
-                                class="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                                <span class="sr-only">View notifications</span>
-                                <BellIcon class="h-6 w-6" aria-hidden="true" />
-                            </button>
+                            
 
                             <!-- Profile dropdown -->
                             <Menu as="div" class="ml-3 relative">
@@ -44,11 +40,9 @@
                                     leave-to-class="transform opacity-0 scale-95">
                                     <MenuItems
                                         class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                        <MenuItem v-for="item in userNavigation" :key="item.name" v-slot="{ active }">
-                                        <a :href="item.href"
-                                            :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">{{
-                                                    item.name
-                                            }}</a>
+                                        <MenuItem v-slot="{ active }">
+                                        <a @click="logout"
+                                            :class="[ 'block px-4 py-2 text-sm text-gray-700']">Sign out</a>
                                         </MenuItem>
                                     </MenuItems>
                                 </transition>
@@ -82,16 +76,12 @@
                             <div class="text-base font-medium leading-none text-white">{{ user.name }}</div>
                             <div class="text-sm font-medium leading-none text-gray-400">{{ user.email }}</div>
                         </div>
-                        <button type="button"
-                            class="ml-auto bg-gray-800 flex-shrink-0 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                            <span class="sr-only">View notifications</span>
-                            <BellIcon class="h-6 w-6" aria-hidden="true" />
-                        </button>
+                       
                     </div>
                     <div class="mt-3 px-2 space-y-1">
-                        <DisclosureButton v-for="item in userNavigation" :key="item.name" as="a" :href="item.href"
-                            class="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700">
-                            {{ item.name }}</DisclosureButton>
+                        <DisclosureButton  as="a" @click="logout"
+                            class="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700 cursor-pointer">
+                            Sign out</DisclosureButton>
                     </div>
                 </div>
             </DisclosurePanel>
@@ -107,16 +97,13 @@ import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuIt
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/vue/outline'
 import {useStore} from 'vuex'
 import {computed} from 'vue'
+import {useRouter} from 'vue-router'
 
 const navigation = [
     { name: 'Dashboard', to:{name:'Dashboard'} },
     { name: 'Surveys',to:{name:"Surveys"}},
 ]
-const userNavigation = [
-    { name: 'Your Profile', href: '#' },
-    { name: 'Settings', href: '#' },
-    { name: 'Sign out', href: '#' },
-]
+
 export default{
     components: {
         Disclosure,
@@ -132,10 +119,20 @@ export default{
     },
     setup(){
         const store = useStore();
+        const router = useRouter();
+
+        function logout(){
+            store.commit('logout');
+            router.push({
+                name:'Login'
+            })
+        }
+
         return{
             user: computed(()=>store.state.user.data),
             navigation,
-            userNavigation,
+            logout
+
 
         };
     },
