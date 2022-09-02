1a68d0e75fd5ff384500987b998e173e872529d1
Nihad Talal <nihadtalal@gmail.com>
2022-09-02 10:39:47 +0300

    add Survey and Surveyview

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -4,6 +4,8 @@ import Dashboard from '../views/Dashboard.vue';
 
 import Surveys from '../views/Surveys.vue';
 
+import SurveyView from '../views/SurveyView.vue';
+
 import Login from '../views/Login.vue';
 
 import Register from '../views/Register.vue';
@@ -26,6 +28,8 @@ const routes=[
         children:[
             {path:'/dashboard', name:'Dashboard', component: Dashboard},
             {path:'/surveys', name:'Surveys', component: Surveys},
+            {path:'/surveys/create', name:'SurveyCreate', component: SurveyView},
+            {path:'/surveys/:id', name:'SurveyView', component: SurveyView},
             
         ]
     },
--- vue/src/views/Surveys.vue
+++ vue/src/views/Surveys.vue
@@ -1,13 +1,32 @@
 <template>
-  <PageComponent title="Surveys">
-    Content goes here
+  <PageComponent>
+    <template v-slot:header>
+
+      <div class="flex justify-between items-center">
+        <h1 class="text-3xl font-bold text-gray-900">Surveys</h1>
+        <router-link :to="{ name: 'SurveyCreate' }"
+          class="py-2 px-3 text-white bg-emerald-500 rounded-md hover:bg-emerald-600">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+            stroke="currentColor" class="h-4 w-4 -mt-1 inline-block">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+          </svg>
+          Add new Survey
+        </router-link>
+      </div>
+    </template>
+    <div>
+      <pre>{{ surveys }}</pre>
+    </div>
   </PageComponent>
 </template>
 
 <script setup>
+import store from "../store";
+import {computed} from "vue";
 import PageComponent from '../components/PageComponent.vue';
+
+const surveys = computed(()=>store.state.surveys);
 </script>
 
 <style>
-
 </style>
