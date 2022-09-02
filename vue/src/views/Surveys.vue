c58f1bacdac9facab1f0788778a1f19e4f7844db
Nihad Talal <nihadtalal@gmail.com>
2022-09-02 16:38:12 +0300

    Create Survey Form

--- vue/src/views/Surveys.vue
+++ vue/src/views/Surveys.vue
@@ -22,7 +22,7 @@
       <h4 class="mt-4 text-lg font-bold">{{ survey.title }}</h4>
       <div v-html="survey.description" class="overflow-hidden flex-1"></div>
       <div class="flex justify-between items-center mt-3">
-        <router-link :to="{name:'SurveyView',params:{id:survey.id}}" class="flex py-2 px-4 border border-transparent text-sm rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+        <router-link :to="{name:'SurveyView',params: { id: survey.id} }" class="flex py-2 px-4 border border-transparent text-sm rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
         
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-2">
   <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
@@ -55,7 +55,7 @@ const surveys = computed(()=>store.state.surveys);
 function deleteSurvey(survey){
   if(confirm(`Are you sure you want to delete this survey? Operation can't be undone!!`))
   {
-    
+
   }
 }
 
