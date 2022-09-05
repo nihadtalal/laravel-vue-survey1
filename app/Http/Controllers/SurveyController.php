94cb9880ad284a811abdd5bfa941b884de9cb971
Nihad Talal <nihadtalal@gmail.com>
2022-09-06 01:09:44 +0300

    Survey Delete in Vue

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -107,8 +107,16 @@ class SurveyController extends Controller
         if($user->id !== $survey->user_id){
             return abort(403,'Unauthorized action.');
         }
+        
 
         $survey->delete();
+
+         //If there is an old image, delete it
+         if($survey->image){
+            $absolutePath = public_path($survey->image);
+            File::delete($absolutePath);
+        }
+        
         return response('',204);
     }
 
--- vue/src/views/SurveyView.vue
+++ vue/src/views/SurveyView.vue
@@ -5,11 +5,25 @@
                 <h1 class="text-3xl font-bold text-gray-900">
                     {{ route.params.id ? model.title : "Create a Survey" }}
                 </h1>
+
+                <button v-if="route.params.id" type="button" @click="deleteSurvey()"
+                    class="py-2 px-3 text-white bg-red-500 rounded-md hover:bg-red-600">
+
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 -mt-1 inline-block">
+                        <path fill-rule="evenodd"
+                            d="M8.75 1A2.75 2.75 0 006 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 10.23 1.482l.149-.022.841 10.518A2.75 2.75 0 007.596 19h4.807a2.75 2.75 0 002.742-2.53l.841-10.52.149.023a.75.75 0 00.23-1.482A41.03 41.03 0 0014 4.193V3.75A2.75 2.75 0 0011.25 1h-2.5zM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4zM8.58 7.72a.75.75 0 00-1.5.06l.3 7.5a.75.75 0 101.5-.06l-.3-7.5zm4.34.06a.75.75 0 10-1.5-.06l-.3 7.5a.75.75 0 101.5.06l.3-7.5z"
+                            clip-rule="evenodd" />
+                    </svg>
+
+
+                    Delete Survey
+                </button>
+
             </div>
         </template>
 
         <!-- <pre>{{ model }}</pre> -->
-       <div v-if="surveyLoading" class="flex justify-center">Loading...</div>
+        <div v-if="surveyLoading" class="flex justify-center">Loading...</div>
         <form v-else @submit.prevent="saveSurvey">
             <div class="shadow sm:rounded-md sm:overflow-hidden">
                 <!--Survey Fields-->
@@ -106,8 +120,9 @@
                         You don't have any questions created
                     </div>
 
-                    <div v-for="(question,index) in model.questions" :key="question.id">
-                    <QuestionEditor :question="question" :index="index" @change="questionChange" @addQuestion="addQuestion" @deleteQuestion="deleteQuestion"></QuestionEditor>
+                    <div v-for="(question, index) in model.questions" :key="question.id">
+                        <QuestionEditor :question="question" :index="index" @change="questionChange"
+                            @addQuestion="addQuestion" @deleteQuestion="deleteQuestion"></QuestionEditor>
                     </div>
 
                 </div>
@@ -126,7 +141,7 @@
 </template>
 
 <script setup>
-import {v4 as uuidv4 } from "uuid";
+import { v4 as uuidv4 } from "uuid";
 import { ref, watch, computed } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 
@@ -139,7 +154,7 @@ const router = useRouter();
 
 const route = useRoute();
 
-const surveyLoading = computed(()=> store.state.currentSurvey.loading)
+const surveyLoading = computed(() => store.state.currentSurvey.loading)
 
 
 //Create empty survey
@@ -149,16 +164,16 @@ let model = ref({
     status: false,
     description: null,
     image: null,
-    image_url:null,
+    image_url: null,
     expire_date: null,
     questions: [],
 });
 
 //Watch to current survey data change and when this happens we update local model
 watch(
-    ()=>store.state.currentSurvey.data,
-    (newVal, oldVal)=>{
-        model.value={
+    () => store.state.currentSurvey.data,
+    (newVal, oldVal) => {
+        model.value = {
             ...JSON.parse(JSON.stringify(newVal)),
             status: newVal.status !== "draft",
         };
@@ -169,44 +184,43 @@ if (route.params.id) {
     store.dispatch('getSurvey', route.params.id);
 }
 
-function onImageChoose(ev){
+function onImageChoose(ev) {
     const file = ev.target.files[0];
 
     const reader = new FileReader();
-    reader.onload=()=>{
+    reader.onload = () => {
         //The filed to send on backend and apply validations
         model.value.image = reader.result;
 
         //The field to display here
-        model.value.image_url=reader.result;
+        model.value.image_url = reader.result;
     };
     reader.readAsDataURL(file);
 }
 
 
-function addQuestion(index){
-    const newQuestion ={
-        id:uuidv4(),
-        type:"text",
-        question:"",
-        description:null,
-        data:{},
+function addQuestion(index) {
+    const newQuestion = {
+        id: uuidv4(),
+        type: "text",
+        question: "",
+        description: null,
+        data: {},
     };
 
     model.value.questions.splice(index, 0, newQuestion);
 }
 
-function deleteQuestion(question){
+function deleteQuestion(question) {
     model.value.questions = model.value.questions.filter(
         (q) => q !== question
     );
 }
 
-function questionChange(question){
+function questionChange(question) {
     model.value.questions = model.value.questions.map(
-        (q)=>{
-            if(q.id == question.id)
-            {
+        (q) => {
+            if (q.id == question.id) {
                 return JSON.parse(JSON.stringify(question));
             }
             return q;
@@ -217,15 +231,31 @@ function questionChange(question){
 /**
  * Create or update Survey
  */
-function saveSurvey(){
-    store.dispatch("saveSurvey",model.value).then(({data}) =>{
+function saveSurvey() {
+    store.dispatch("saveSurvey", model.value).then(({ data }) => {
         router.push({
-            name:"SurveyView",
-            params:{id: data.data.id},
+            name: "SurveyView",
+            params: { id: data.data.id },
 
         })
     });
 }
+
+function deleteSurvey(){
+    if(
+        confirm(
+            `Are you sure you want to delete this survey? Operation can't be undone`
+        )
+
+    ){
+        store.dispatch("deleteSurvey", model.value.id).then(() =>{
+            router.push({
+                name:"Surveys",
+            })
+        });
+    }
+}
+
 </script>
 
 <style>
